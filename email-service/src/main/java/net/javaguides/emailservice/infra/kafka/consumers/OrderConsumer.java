4d2973492adc44653939971b1d1e4f2713f6fd78
Elzio Lima <elziolimach@gmail.com>
2024-05-16 14:32:01 -0400

    feat: add infra layer OrderConsumer to email service

---
+++ email-service/src/main/java/net/javaguides/emailservice/infra/kafka/consumers/OrderConsumer.java
@@ -0,0 +1,28 @@
+package net.javaguides.emailservice.infra.kafka.consumers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import net.javaguides.basedomains.dto.OrderEvent;
+
+@Service
+public class OrderConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(
+        topics="${spring.kafka.topic.name}",
+        groupId="${spring.kafka.consumer.group-id}"
+    )
+    private void consume(OrderEvent event) {
+        LOGGER.info(
+            String.format(
+                "Order event received in email service => %s",
+                event.toString()
+            )
+        );
+
+        // save the order event in database
+    }
+}
